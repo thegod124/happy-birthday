f88bf4c3c55d6c4e409e07aec2a1016b0e55bb6c
thegod124 <81662245+thegod124@users.noreply.github.com>
2021-09-07 20:03:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var canvas = new fabric.Canvas('myCanvas');
 var x= document.getElementById("myAudio");
 
 function new_image(){
-    fabric.Image.fromURL("BirthdayImage.jpg", function(Img){
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
         block_image_object = Img;
         block_image_object.scaleToWidth(700);
-        block_image_object.scaleToHeight(150);
+        block_image_object.scaleToHeight(510);
         block_image_object.set({
             top:0,
             left:0
@@ -15,5 +15,5 @@ function new_image(){
 }
 
 function playsound(){
-	x.play("bdayaudio.mp3");
+	x.play();
 }
